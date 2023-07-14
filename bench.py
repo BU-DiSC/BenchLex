a5f085a35d4b1581353c89df43da149a6cfbf7fe
ndhuynh <huynh.n.andy@gmail.com>
2023-07-14 11:42:15 -0400

    Add first argument for benchmark entry point

--- bench.py
+++ bench.py
@@ -1,10 +1,21 @@
 import argparse
 from benchlex.alexpy import AlexPy
 
+def load_args():
+    parser = argparse.ArgumentParser(description='Benchmark for ALEX')
+    parser.add_argument('--keys_file', help='keys file', required=True)
+
+    args = parser.parse_args()
+
+    return args
+
 
 def main() -> int:
+    args = load_args()
+
     alex = AlexPy()
     alex.bench()
+
     return 0
 
 
