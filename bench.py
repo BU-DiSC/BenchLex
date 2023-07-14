fb1004fb55458d92640de8c17d8babfa6535129b
clarhenz <122268916+clarhenz@users.noreply.github.com>
2023-07-14 14:10:34 -0400

    Update bench.py

--- bench.py
+++ bench.py
@@ -29,13 +29,7 @@ def output(result):
     out = tuple(int(float(i.split()[0])) for i in out)
     return out
 
-def main(x = "./build/benchmark \
---keys_file=resources/sample_keys.bin \
---keys_file_type=binary \
---init_num_keys=500 \
---total_num_keys=1000 \
---batch_size=1000 \
---insert_frac=0.5"):
+def main():
     args = loadArgs()
     result = call_benchmark(args)
     out = output(result)
