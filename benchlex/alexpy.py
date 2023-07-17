1240ecdb7b8771236599f5e0314fc822b1869b64
clarhenz <clarahenzinger@gmail.com>
2023-07-17 09:56:27 -0400

    Add lookup distribution

--- benchlex/alexpy.py
+++ benchlex/alexpy.py
@@ -10,13 +10,14 @@ Python wrapper around ALEX to call and record values from ALEX
 """
 
 class AlexPy():
-    def __init__(self,keys_file,keys_file_type,init_num_keys,total_num_keys,batch_size,insert_frac):
+    def __init__(self,keys_file,keys_file_type,init_num_keys,total_num_keys,batch_size,insert_frac, lookup_distribution):
         self.keys_file = keys_file 
         self.keys_file_type = keys_file_type
         self.init_num_keys = init_num_keys
         self.total_num_keys = total_num_keys
         self.batch_size = batch_size
         self.insert_frac = insert_frac
+        self.lookup_distribution = lookup_distribution
         
 
     def run(self):
@@ -28,13 +29,14 @@ class AlexPy():
         --init_num_keys={self.init_num_keys} \
         --total_num_keys={self.total_num_keys} \
         --batch_size={self.batch_size} \
-        --insert_frac={self.insert_frac}"
+        --insert_frac={self.insert_frac}\
+        --lookup_distribution={self.lookup_distribution}"
         result = subprocess.run(cmd, shell=True, capture_output=True, text=True).stdout
         return result.splitlines()[-1]
 
     # def bench(self):
     def main(self):
-        aResult = AlexPy(self.keys_file,self.keys_file_type,self.init_num_keys,self.total_num_keys,self.batch_size,self.insert_frac)
+        aResult = AlexPy(self.keys_file,self.keys_file_type,self.init_num_keys,self.total_num_keys,self.batch_size,self.insert_frac, self.lookup_distribution)
         result = AlexPy.run(aResult)
         out = output(result)
         print(out)
