bbabfb21b8159b802d39f08eb5f9bd7e5f5ce420
clarhenz <clarahenzinger@gmail.com>
2023-07-17 10:01:43 -0400

    Set result as a variable

--- benchlex/experiment.py
+++ benchlex/experiment.py
@@ -1,5 +1,7 @@
 import alexpy
 
 if __name__ == "__main__":
+    #default
     experiment1 = alexpy.AlexPy("resources/sample_keys.bin","binary",500,1000,1000,0.5,"uniform")
-    experiment1.main()
+    result = experiment1.main()
+    print(result)
