0e0616cb6c3aaa81c459be1923e1bef503d0c0fe
clarhenz <clarahenzinger@gmail.com>
2023-07-17 09:12:12 -0400

    created functional experiment file

--- benchlex/experiment.py
+++ benchlex/experiment.py
@@ -1,2 +1,5 @@
-def experiment1():
-    return
+import alexpy
+
+if __name__ == "__main__":
+    experiment1 = alexpy.AlexPy("resources/sample_keys.bin","binary",500,1000,1000,0.5)
+    experiment1.main()
