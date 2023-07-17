9b11dd71035192b420f8d9839bd76e3ccdfb13a2
clarhenz <clarahenzinger@gmail.com>
2023-07-17 10:14:22 -0400

    Create function that runs ALEX x times

--- benchlex/experiment.py
+++ benchlex/experiment.py
@@ -1,7 +1,16 @@
 import alexpy
 
+def write(res,filename):
+    with open(filename, 'a') as f:
+        f.write(str(res) + "\n")
+        f.close()
+
+def runThrough(x):
+    for i in range(x):
+        experiment1 = alexpy.AlexPy("resources/sample_keys.bin","binary",500,1000,1000,0.5,"uniform")
+        result = experiment1.main()
+        write(result,"output.txt")
+
 if __name__ == "__main__":
     #default
-    experiment1 = alexpy.AlexPy("resources/sample_keys.bin","binary",500,1000,1000,0.5,"uniform")
-    result = experiment1.main()
-    print(result)
+    runThrough(10)
