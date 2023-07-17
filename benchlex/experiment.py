f16ae34c9ef75cc5e022ed79dd68521322db3d67
clarhenz <clarahenzinger@gmail.com>
2023-07-17 09:56:45 -0400

    Add lookup distribution

--- benchlex/experiment.py
+++ benchlex/experiment.py
@@ -1,5 +1,5 @@
 import alexpy
 
 if __name__ == "__main__":
-    experiment1 = alexpy.AlexPy("resources/sample_keys.bin","binary",500,1000,1000,0.5)
+    experiment1 = alexpy.AlexPy("resources/sample_keys.bin","binary",500,1000,1000,0.5,"uniform")
     experiment1.main()
