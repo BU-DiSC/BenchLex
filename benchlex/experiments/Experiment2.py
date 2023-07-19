b7573fc85cf9a47d36c7814e125572b001e89fca
clarhenz <clarahenzinger@gmail.com>
2023-07-19 14:01:00 -0400

    Add empty experiment files

---
+++ benchlex/experiments/Experiment2.py
@@ -0,0 +1,8 @@
+import alexpy
+import os
+import experiment
+class Experiment2():
+    def __init__(self) -> None:
+        pass
+    def run(self):
+        pass
---
+++ benchlex/experiments/Experiment4.py
@@ -0,0 +1,8 @@
+import alexpy
+import os
+import experiment
+class Experiment4():
+    def __init__(self) -> None:
+        pass
+    def run(self):
+        pass
