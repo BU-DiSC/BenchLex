20cd38704732c4f35bb963139bceb29b5c0a1068
savirb <savir.basil@gmail.com>
2023-07-20 10:14:48 -0400

    Fix errors

--- benchlex/alexpy.py
+++ benchlex/alexpy.py
@@ -7,7 +7,7 @@ Python wrapper around ALEX to call and record values from ALEX
 """
 
 class AlexPy():
-    def __init__(self,keys_file,keys_file_type,init_num_keys,total_num_keys,batch_size,insert_frac, lookup_distribution):
+    def __init__(self,keys_file,keys_file_type,init_num_keys,total_num_keys,batch_size,insert_frac, lookup_distribution, user):
         self.keys_file = keys_file 
         self.keys_file_type = keys_file_type
         self.init_num_keys = init_num_keys
@@ -15,13 +15,10 @@ class AlexPy():
         self.batch_size = batch_size
         self.insert_frac = insert_frac
         self.lookup_distribution = lookup_distribution
-    
-    def user():
-        user =  input("user: ")
-        return user
+        self.user = user
 
     def run(self):
-        path = "/home/" + AlexPy.user() + "/ALEX/build/benchmark"
+        path = "/home/" + self.user + "/ALEX/build/benchmark"
         cmd = path + f"\
         --keys_file={self.keys_file} \
         --keys_file_type={self.keys_file_type} \
@@ -45,7 +42,7 @@ class AlexPy():
 
     # def bench(self):
     def main(self):
-        aResult = AlexPy(self.keys_file,self.keys_file_type,self.init_num_keys,self.total_num_keys,self.batch_size,self.insert_frac, self.lookup_distribution)
+        aResult = AlexPy(self.keys_file,self.keys_file_type,self.init_num_keys,self.total_num_keys,self.batch_size,self.insert_frac, self.lookup_distribution, self.user)
         result = AlexPy.run(aResult)
         out = AlexPy.output(result)
         return out
--- benchlex/experiment.py
+++ benchlex/experiment.py
@@ -2,23 +2,24 @@ import alexpy
 import os
 
 class experiment():
-    def __init__(self,lookup_distribution):
+    def __init__(self,lookup_distribution, user):
         self.lookup_distribution = lookup_distribution
+        self.user = user
         
     def write(res,filename):
         with open(filename, 'a') as f:
             f.write(str(res) + "\n")
             f.close()
 
-    def runThrough(self,x):
-        filename = "benchlex/output.txt"
+    def runThrough(self, x):
+        filename = "/home/sbasil/BenchLex/benchlex/output.txt"
         if os.path.isfile(filename):
             os.remove(filename)
         lookups = []
         inserts = []
         ops = []
         for i in range(x):
-            experiment1 = alexpy.AlexPy("/home/"+ alexpy.user() +"/BenchLex/resources/sample_keys.bin","binary",500,1000,1000,0.5,self.lookup_distribution)
+            experiment1 = alexpy.AlexPy("/home/"+ self.user +"/BenchLex/resources/sample_keys.bin","binary",500,1000,1000,0.5,self.lookup_distribution, self.user)
             result = experiment1.main()
             experiment.write(result,filename)
             lookups.append(result[0])
@@ -26,6 +27,7 @@ class experiment():
             ops.append(result[2])
         return sum(lookups)/len(lookups), sum(inserts)/len(inserts), sum(ops)/len(ops)
 
-    if __name__ == "__main__":
-        #default
-        print(runThrough(100))
+if __name__ == "__main__":
+    #default'
+    test = experiment("uniform", "sbasil")
+    print(test.runThrough(1))
