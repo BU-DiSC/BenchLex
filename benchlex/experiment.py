6932d50e6db44b5dc9e115c378f05980ac023960
savirb <savir.basil@gmail.com>
2023-07-21 10:04:54 -0400

    Analyze K

--- benchlex/experiment.py
+++ benchlex/experiment.py
@@ -1,47 +1,72 @@
 import alexpy
 import os
 import csv
+import subprocess
 
 class experiment():
-    def __init__(self,lookup_distribution, user, insert_frac):
+    def __init__(self,lookup_distribution, user, insert_frac, keys_file, keys_file_type, K, L):
+        self.keys_file = keys_file
+        self.keys_file_type = keys_file_type
         self.lookup_distribution = lookup_distribution
         self.user = user
         self.insert_frac = insert_frac
+        self.K = K
+        self.L = L
         
-    def write(res,filename):
-        with open(filename, 'a') as f:
-            f.write(str(res) + "\n")
-            f.close()
+    #def write(res,filename):
+        #with open(filename, 'a') as f:
+            #f.write(str(res) + "\n")
+            #f.close()
 
     def runThrough(self, x):
-        filename = f"/home/{self.user}/BenchLex/benchlex/output.txt"
-        if os.path.isfile(filename):
-            os.remove(filename)
+        #filename = f"/home/{self.user}/BenchLex/benchlex/output.txt"
+        #if os.path.isfile(filename):
+            #os.remove(filename)
 
         lookups = []
         inserts = []
         ops = []
         for i in range(x):
-            experiment1 = alexpy.AlexPy("/home/"+ self.user +"/BenchLex/resources/sample_keys.bin","binary",500,1000,1000,self.insert_frac,self.lookup_distribution, self.user)
+            experiment1 = alexpy.AlexPy(self.keys_file,self.keys_file_type,500,1000,1000,self.insert_frac,self.lookup_distribution, self.user)
             result = experiment1.main()
-            experiment.write(result,filename)
+            #experiment.write(result,filename)
             lookups.append(result[0])
             inserts.append(result[1])
             ops.append(result[2])
-        return [self.insert_frac, sum(lookups)/x, sum(inserts)/x, sum(ops)/x]
+        return [self.K, self.L, sum(lookups)/x, sum(inserts)/x, sum(ops)/x] #cast K,L to int
 
+
+    def createKeysFile(self):
+        path = "/home/" + self.user + "/bods/src/sortedness_data_generator"
+        cmd = path + f"\
+        -N 1000 \
+        -K {self.K} \
+        -L {self.L} \
+        -S 1 \
+        -o /home/{self.user}/bods/workloads/createdata_N1000_K{self.K}_L{self.L}_S1234_a1_b1_P4.txt \
+        -a 1\
+        -b 1\
+        -P 4"
+        subprocess.run(cmd, shell=True, capture_output=True, text=True)
+        
 if __name__ == "__main__":
     #default'
     user = input("user: ")
 
+    #for i in range(101):
+        #test = experiment("uniform", user, 0.5, f"/home/sbasil/bods/workloads/createdata_N1000_K{1}_L{1}_S1234_a1_b1_P4.txt","text", 1, 1)
+        #test.createKeysFile()
+        #print(test.runThrough(5))
 
+    
     #writing to csv
-    fields = ["insert_frac", "lookups/sec", "inserts/sec", "ops/sec"]
+    fields = ["K","L", "lookups/sec", "inserts/sec", "ops/sec"]
     rows = []
     filename = "data.csv"
-    for i in range(1, 200): # i / 100 is insert frac
-        test = experiment("uniform", user, i / 200)
-        rows.append(test.runThrough(1000)) #run each insert_frac 1000 times, calc mean
+    for i in range(101): # i / 100 is insert frac
+        test = experiment("uniform", user, 0.5, f"/home/sbasil/bods/workloads/createdata_N1000_K{i}_L10_S1234_a1_b1_P4.txt","text", i, 10)
+        test.createKeysFile()
+        rows.append(test.runThrough(100)) #run each insert_frac 1000 times, calc mean
         print(i)
     
     with open(filename, 'w') as csvfile: 
@@ -53,3 +78,5 @@ if __name__ == "__main__":
         
         # writing the data rows 
         csvwriter.writerows(rows)
+
+    
