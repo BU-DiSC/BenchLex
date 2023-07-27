5a41052b12e9a5e02dadaa6cf565a83585e14d6f
clarhenz <clarahenzinger@gmail.com>
2023-07-27 19:12:48 -0400

    Adding loadtime

--- benchlex/experiments/Experiment1.py
+++ benchlex/experiments/Experiment1.py
@@ -13,16 +13,16 @@ class Experiment1():
         from experiment import experiment
         print("Running Experiment1...")
 
-        fields = ["K","L", "lookups/sec", "inserts/sec", "ops/sec"]
+        fields = ["K","L", "lookups/sec", "inserts/sec", "ops/sec", "loadtime"]
         rows = []
         filename = "data.csv"
         for k in range(101):
             for l in range(101):
                 test = experiment("uniform", self.user, 0.5, f"/home/{self.user}/bods/workloads/createdata_N{10000}_K{k}_L{l}_S1234_a1_b1_P4.txt","text", k, l, 10000)
                 test.createKeysFile()
-                rows.append(test.runThrough(100)) #run each insert_frac 10 times, calc mean
+                rows.append(test.runThrough(10)) #run each insert_frac 10 times, calc mean
                 print(k, l)
-    
+        
         with open(filename, 'w') as csvfile: 
             # creating a csv writer object 
             csvwriter = csv.writer(csvfile) 
