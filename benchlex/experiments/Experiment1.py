2fb155e7dfc0c45b1d00ff6038bafca6637c186b
clarhenz <122268916+clarhenz@users.noreply.github.com>
2023-08-03 20:19:19 -0400

    Update Experiment1.py

--- benchlex/experiments/Experiment1.py
+++ benchlex/experiments/Experiment1.py
@@ -15,13 +15,14 @@ class Experiment1():
 
         fields = ["K","L", "lookups/sec", "inserts/sec", "ops/sec", "loadtime"]
         rows = []
-        filename = "data.csv"
-        for k in range(101):
-            for l in range(101):
+        filename = "data10.csv"
+        N = 1000000
+        for k in range(0,101,10):
+            for l in range(0,101,10):
                 if not(k == 7 and l == 60):
-                    test = experiment("uniform", self.user, 0.5, f"/home/{self.user}/bods/workloads/createdata_N{1000000}_K{k}_L{l}_S1234_a1_b1_P4.txt","text", k, l, 1000000)
+                    test = experiment("uniform", self.user, 0.5, f"/home/{self.user}/bods/workloads/createdata_N{N}_K{k}_L{l}_S1234_a1_b1_P0.txt","text", k, l, N)
                     test.createKeysFile()
-                    rows.append(test.runThrough(10)) #run each insert_frac 10 times, calc mean
+                    rows.append(test.runThrough(2)) #run each insert_frac 10 times, calc mean
                     print(k, l)
         
         with open(filename, 'w') as csvfile: 
